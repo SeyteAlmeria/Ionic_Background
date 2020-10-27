7bcf5a47366483e803cc09b52b04de5959c65809
Servicios Empresariales y T.SL <serviciosempresarialesytsl@Mac-mini-de-Servicios.local>
2020-10-27 15:40:58 +0100

    sets background mode with https, haptics and gps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core'
 import { BackgroundMode } from '@ionic-native/background-mode/ngx'
 import { Plugins } from '@capacitor/core'
 import { Platform } from '@ionic/angular'
+import { HttpClient } from '@angular/common/http'
 
-const { Haptics } = Plugins
+const { Haptics, Geolocation } = Plugins
 
 @Component({
     selector: 'app-home',
@@ -11,9 +12,14 @@ const { Haptics } = Plugins
     styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-    constructor(public backgroundMode: BackgroundMode, private platform: Platform) {
+    constructor(public backgroundMode: BackgroundMode, private platform: Platform, private http: HttpClient) {
         this.platform.ready().then(() => {
-            this.backgroundMode.enable()
+            this.backgroundMode.enable();
+
+            this.backgroundMode.on('activate').subscribe(() => {
+                console.log('background mode activated event');
+                this.backgroundMode.disableWebViewOptimizations();
+            });
 
             console.log('background mode enabled?', this.backgroundMode.isEnabled())
             console.log('background mode active?', this.backgroundMode.isActive())
@@ -21,7 +27,23 @@ export class HomePage {
                 console.log('background mode enabled?', this.backgroundMode.isEnabled())
                 console.log('background mode active?', this.backgroundMode.isActive())
                 Haptics.vibrate()
-            }, 10 * 1000)
+                Geolocation.getCurrentPosition({
+                    enableHighAccuracy: false,
+                    timeout: 1000,
+                }).then(coordinates => {
+                    const p = {
+                        latitude: coordinates.coords.latitude,
+                        longitude: coordinates.coords.longitude,
+                    };
+                    console.log(p);
+                }).catch(e => {
+                    console.log(e);
+                });
+
+                this.http.get('http://dummy.restapiexample.com/api/v1/employee/1').subscribe((res) => {
+                    console.log(res);
+                });
+            }, 5 * 1000)
 
             this.backgroundMode.on('enable').subscribe(() => {
                 console.log('background mode enabled event')
